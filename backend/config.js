c335c47977bc82bf208b4cd0e7228e80fdec5eb4
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-01 15:07:11 -0700

    Trying to hide API keys :(

--- backend/config.js
+++ backend/config.js
@@ -9,12 +9,6 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
-// API keys for TMDB
-// https://developers.themoviedb.org/3/movies/get-movie-details
-const API_KEY_V3 = "2ce92882d951f47a76b948943e739de5";
-const API_KEy_V4 = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyY2U5Mjg4MmQ5NTFmNDdhNzZiOTQ4OTQzZTczOWRlNSIsInN1YiI6IjYwYjZhOTk4ZGQ5MjZhMDAyOTQ5MWQ1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ccb3rKE2GxzY5Jh5Vgh2MwKACfjJBAvtkx5Wn0QcO9A"
-
-
 // Choose proper database based on context (dev, testing, or production)
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
@@ -33,5 +27,5 @@ module.exports = {
   PORT,
   getDatabaseUri,
   API_KEY_V3,
-  API_KEy_V4,
+  API_KEY_V4,
 };
