cebdaced405d26fd988733495c4e5730f3f402c8
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-03 09:10:24 -0700

    Add to addVote static method.

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -10,27 +10,36 @@ const {
 
 class Movie {
   /** adds a vote to a movie
+   * @param id integer
+   * @param vote string: "positive" or "negative"
+   * @returns { title, positive, negative }
    * 
-   * Returns { title, positive, negative }
-   * 
+   * Throws NotFoundError if movie not found.
    */
 
-  static async addVote(id, vote, movie) {
-    // Try to find movie, if no movie, add movie, then add vote
+  static async addVote(id, vote) {
     const movieRes = await db.query(
-      `SELECT *
-      FROM ratings
-      WHERE id = $1`,
-      [id],
+          `SELECT *
+          FROM ratings
+          WHERE id = $1`,
+          [id],
     );
     
-    const localMovie = movieRes.rows[0];
+    const movie = movieRes.rows[0];
 
-    if (!localMovie) this.addMovie(movie);
-  }
+    if (!movie) throw new NotFoundError(`No movie with ID: ${id}`);
 
-  static async addMovie(movie) {
+    const voteRes = await db.query(
+          `UPDATE ratings
+          SET $1 = $1 + 1
+          WHERE id = $2
+          RETURNING title,
+                    positive,
+                    negative`,
+          [vote, id],
+    );
 
+    return voteRes;
   }
 }
 
