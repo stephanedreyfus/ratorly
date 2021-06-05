bc93de877a828a1b57e09e377f15d2e35df1a0bd
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-04 21:09:25 -0700

    Add vote post route.

--- backend/routes/rate.js
+++ backend/routes/rate.js
@@ -3,11 +3,12 @@
 /** Routes for rating movies. */
 
 const express = require("express");
-const { BarRequestError } = require("../expressError");
+const { BarRequestError, BadRequestError } = require("../expressError");
 const Movie = require("../models/movie");
 
 const router = express.Router({ mergeParams: true });
 
+// This temporary route will be replaced by front end page.
 router.get("/", async function (req, res, next) {
   try {
     // Api call to get some movies goes here
@@ -16,6 +17,30 @@ router.get("/", async function (req, res, next) {
     return next(err);
   }
 });
+
+
+/** POST / { rating, movie } => { rating } OR { rating, movie }
+ * 
+ * incoming data should be { rate, movie_id, title, release_date, poster_path }
+ * 
+ * Returns { title, positive, negative }
+*/
+router.post("/"), async function (req, res, next) {
+  try {
+    if (
+        !req.body ||
+        (req.body.rating !== "positive" &&
+        req.body.rating !== "negative")
+       ) {
+         throw new BadRequestError("Improper rating data.");
+       }
+    
+    const votes = await Movie.addVote(req.body);
+    return res.status(200).json({ votes });
+  } catch (err) {
+    return next(err);
+  }
+}
 
 module.exports = router;
 
