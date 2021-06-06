715755248c6fc08b8eabd4f09c351ae6102c1fac
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-05 19:45:51 -0700

    Rethinking routes and models.

--- backend/routes/rate.js
+++ backend/routes/rate.js
@@ -7,6 +7,17 @@ const { BadRequestError } = require("../expressError");
 const Movie = require("../models/movie");
 
 const router = express.Router({ mergeParams: true });
+  
+
+// This temporary route will be replaced by front end page.
+router.get("/", async function (req, res, next) {
+  try {
+    // Api call to get some movies goes here
+    return res.send('<h2>The movies page! Will populate soon!</h2>')
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
 /** POST / { rating, movie } => { rating } OR { rating, movie }
@@ -15,7 +26,7 @@ const router = express.Router({ mergeParams: true });
  * 
  * Returns { title, positive, negative }
  */
-router.post("/"), async function (req, res, next) {
+router.post("/", async function (req, res, next) {
   try {
     if (
       !req.body ||
@@ -29,20 +40,9 @@ router.post("/"), async function (req, res, next) {
       return res.status(200).json({ ratings });
     } catch (err) {
       return next(err);
-    }
   }
-  
+});
 
-  // This temporary route will be replaced by front end page.
-  router.get("/", async function (req, res, next) {
-    try {
-      // Api call to get some movies goes here
-      return res.send('<h2>The movies page! Will populate soon!</h2>')
-    } catch (err) {
-      return next(err);
-    }
-  });
 
   module.exports = router;
-
-// When voting and searching for a movie, remember to send id as an integer!
+// When voting and searching for a movie, remember to send movie_id as an integer!
