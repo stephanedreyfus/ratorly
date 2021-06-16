99e4f7a97c6720082c7a11193f4a7ef104f82022
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-16 16:44:18 -0700

    Begin front App.js setup.

--- ratorly-front/src/App.js
+++ ratorly-front/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter} from "react-router-dom";
+import Navigation from ".Navigation";
+import Routes from "./Routes";
+import RatorlyApi from "./RatorlyApi";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Eventually search for and rate movies.</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <p>Eventually search for and rate movies.</p>
+        <Navigation />
+        <Routes />
+      </div>
+    </BrowserRouter>
   );
 }
 
