ed4db21ea44e6975e8cdd18305701b07637c333b
Stéphane Dreyfus <floatingrock@gmail.com>
2021-06-22 15:18:34 -0700

    Add TMDb basics to ratorly API.

--- ratorly-front/src/App.js
+++ ratorly-front/src/App.js
@@ -1,10 +1,28 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter} from "react-router-dom";
 import Navigation from ".Navigation";
 import Routes from "./Routes";
 import RatorlyApi from "./RatorlyApi";
 import './App.css';
+import { ClipLoader } from "react-spinners";
 
 function App() {
+  const [currentMovies, setCurrentMovies]
+  const [moviesLoaded, setMoviesLoaded] = useState(false);
+
+  // Collect movies to display from TMDb
+  useEffect(() => {
+    async function getMovies {
+      try {
+        let moviesToShow = await RatorlyApi.getCurrentMovies()
+        setCurrentMovies(moviesToShow);
+        setMoviesLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+
   return (
     <BrowserRouter>
       <div className="App">
--- ratorly-front/src/RatorlyApi.js
+++ ratorly-front/src/RatorlyApi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = process.env.BASE_URL || "http://localhost:3001";
+const BASE_TMDb_URL = "https://api.themoviedb.org/3/movie";
 
 class RatorlyApi {
+  // Requests related to Ratorly back end.
   static async request(endpoint, params = {}, verb = "get") {
 
     console.debug("API Call:", endpoint, params, verb);
@@ -21,8 +23,8 @@ class RatorlyApi {
       return await q;
     } catch (err) {
       console.error("API Error:", err.response);
-     let message = err.response.data.message;
-     throw Array.isArray(message) ? message : [message]; 
+      let message = err.response.data.message;
+      throw Array.isArray(message) ? message : [message]; 
     }
   }
 
@@ -43,7 +45,39 @@ class RatorlyApi {
 
   static async addRating(data) {
     let res = await this.request("rate", data, "post");
-    return res.rating;
+    return res.ratings;
+  }
+
+  // Requests related to TMDb API
+  static async dbRequest(endpoint, params = {}) {
+    console.debug("TMDb Call:", endpoint, params);
+
+    let q;
+    // Else needs updating from searhch type available at TMDb
+    // Need to check how TMDb handles searches.
+    if (endpoint === "now_playing") {
+      q = axios.get(
+        `${BASE_TMDb_URL}/${endpoint}`
+      );
+    } else {
+      q = axios.get(
+        `${BASE_TMDb_URL}/${endpoint}`, { params: { params }}
+      );
+    }
+
+    try {
+      return await q;
+    } catch (err) {
+      console.log ("DB API Error:", err.response);
+      let message = err.response.data.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+ 
+  // Returns an array of 20 movies currently showing.
+  static async getCurrentMovies() {
+    let res = await this.dbRequest("now_playing");
+    return res.results;
   }
 }
 
