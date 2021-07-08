1caabff376a2dbc60f3eacb2e4d54b17295e3edb
Stéphane Dreyfus <floatginrock@gmail.com>
2021-07-08 10:11:31 -0700

    Edit MovieCard.

--- ratorly-front/src/movies/MovieCard.js
+++ ratorly-front/src/movies/MovieCard.js
@@ -3,20 +3,16 @@ import "./MovieCard.css";
 
 function MovieCard({ title, positive, negative, poster }) {
     // TODO Look for good fontawesome for ratings <p>
-    return ( <
-        div className = "movie-card container" >
-        <
-        img src = { poster }
-        alt = { title }
-        /> <
-        div className = "movie-title" > { title } < /div> <
-        div >
-        Ratings:
-        <
-        p > { positive } < /p> <
-        p > { negative } < /p> <
-        /div> <
-        /div>
+    return (
+        <div className="movie-card container">
+            <img src={poster} alt={title} />
+            <div className="movie-title">{title}</div>
+            <div>
+                Ratings:
+                <p>{positive}</p>
+                <p>{negative}</p>
+            </div>
+        </div>
     )
 }
 
